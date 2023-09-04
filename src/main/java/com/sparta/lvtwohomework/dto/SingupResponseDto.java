6d9454b3b27bde96530df5ee88de3befc49ae8c9
SangYoungBaek <bsy2274@naver.com>
2023-09-04 09:32:00 +0900

    Lv2 commit

---
+++ src/main/java/com/sparta/lvtwohomework/dto/SingupResponseDto.java
@@ -0,0 +1,13 @@
+package com.sparta.lvtwohomework.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SingupResponseDto {
+    private String status;
+    private String msg;
+
+
+}
